47ee8982771947864946cd03320c824371f2a0fc
RY6423 <tauryo.31661144.nitayu@gmail.com>
2021-06-21 01:51:28 +0900

    Use macro for iteration

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 
 #include <iostream>
 
+#define ITERATTION 500
+
 int main()
 {
-    for (int i = 0; i < 500; i++) {
+    for (int i = 0; i < ITERATTION; i++) {
         std::cout << i << std::endl;
     }
     return 0;
