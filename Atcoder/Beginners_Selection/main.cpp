63f897727718bbb9fc9a71d1735c89bdcc0ad406
RY6423 <tauryo.31661144.nitayu@gmail.com>
2021-07-06 12:39:05 +0900

    placing_marbles

---
+++ Atcoder/Beginners_Selection/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+    int count = 0;
+    for (int i = 0; i < 3; i++) {
+        if (s[i] == '1') {
+            count++;
+        }
+    }
+    std::cout << count << std::endl;
+}
